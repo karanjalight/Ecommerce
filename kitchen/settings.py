092d25060b638b6e3186f2223dbf1f8c42992b0b
PAUL <you@example.com>
2021-10-29 13:50:09 +0300

    correction2 heroku db connection

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
 url = 'postgres://jyuixnyoefqwcd:6c4faf6c0e81622d312d9e01d90e263049c8e36f1a1b715bd0c7e3f5c6b79f5'
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'] = dj_database_url.config(default=url)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
